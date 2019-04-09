ef98ab9faf5837f3737710b4ff481e175890ce36
jpchionglo <jpchionglo@gmail.com>
2019-04-09 16:33:03 -0500

    Add files via upload

--- Script.sql
+++ Script.sql
@@ -1,6 +1,6 @@
 create type privilege as ENUM('admin', 'scheduler', 'medicalStaff', 'patient');
 create type category as ENUM('Lab', 'MRI', 'Xray', 'Office Visit');
-
+create type jobtype as ENUM('Medical Staff', 'Admin', 'Scheduler');
 
 create table login (
 
@@ -17,7 +17,7 @@ create table login (
 create table diagnostic (
 
 	ID varchar(16),
-	rice decimal(6,2),
+	price decimal(6,2),
 	cat category,
 	primary key (ID)
 
@@ -25,6 +25,31 @@ create table diagnostic (
 
 create table patient (
 
-	fname char()
+	fname varchar(16),
+	lname varchar(16),
+	address varchar(16),
+	patientID varchar(16)
+
+);
+
+create table employee (
+
+	fname varchar(16),
+	lname varchar(16),
+	staffID varchar(16),
+	job jobtype,
+	primary key (staffID)
+
+);
+
+create table ord (
+
+	orderID varchar(16),
+	customerID varchar(16),
+	staffID varchar(16),
+	diagnosticID varchar(16),
+	results text,
+	primary key (orderID)
+	foreign key (diagnosticID) references diagnostic(ID) 
 
 );

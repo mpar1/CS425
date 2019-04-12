c801b24ff8daef3f6cec47b71235b653038b3aec
mpar1 <mpar1@hawk.iit.edu>
2019-04-11 23:07:47 -0500

    Update Script.sql
    fixed syntax errors. now creates tables, indexes, and roles

--- Script.sql
+++ Script.sql
@@ -1,33 +1,32 @@
-
 create index timestamps on login(LoginTime,LogoutTime);
 create index fullname on patient(fname,lname);
 create index staff_fullname on employee(fname,lname);
 
-create table Login (
+create table login (
 	userID varchar(16),
 	privilege varchar(12),
 	LoginTime time,
 	LogoutTime time,
 	primary key (userID),
-	check (privilege in ('admin', 'scheduler', 'medicalStaff', 'patient')),
+	check (privilege in ('admin', 'scheduler', 'medicalStaff', 'patient'))
 );
 
-create table Diagnostic (
+create table diagnostic (
 	ID varchar(16),
 	price decimal(6,2),
 	category varchar(12),
 	primary key (ID),
-	check (category in ('Lab', 'MRI', 'Xray', 'Office Visit')),
+	check (category in ('Lab', 'MRI', 'Xray', 'Office Visit'))
 );
 
-create table Patient (
+create table patient (
 	fname varchar(16),
 	lname varchar(16),
 	address varchar(16),
 	patientID varchar(16)
 );
 
-create table Order (
+create table Orders (
 	orderID varchar(16),
 	customerID varchar(16),
 	staffID varchar(16),
@@ -35,10 +34,10 @@ create table Order (
 	results text,
 	primary key (orderID),
 	foreign key (diagnosticID) references diagnostic(ID),
-	foreign key (staffID) references employee(staffID),
+	foreign key (staffID) references employee(staffID)
 );
 
-create table Employee (
+create table employee (
 	fname varchar(16),
 	lname varchar(16),
 	staffID varchar(16),
@@ -52,8 +51,8 @@ create role scheduler;
 create role medicalStaff;
 create role patient;
 
-grant select on Order to public;
-grant insert on Order to medicalStaff;
-grant select, update on Patient to medicalStaff;
-grant insert on Patient to admin;
-grant insert on Login to admin;
+grant select on Orders to public;
+grant insert on Orders to medicalStaff;
+grant select, update on patient to medicalStaff;
+grant insert on patient to admin;
+grant insert on login to admin;

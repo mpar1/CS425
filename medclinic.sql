1d8af867dff7ae98b2461830883661e25111b098
jpchionglo <jpchionglo@gmail.com>
2019-04-11 21:06:51 -0500

    added some indexes

---
+++ medclinic.sql
@@ -0,0 +1,60 @@
+create type privilege as ENUM('admin', 'scheduler', 'medicalStaff', 'patient');
+create type category as ENUM('Lab', 'MRI', 'Xray', 'Office Visit');
+create type jobtype as ENUM('Medical Staff', 'Admin', 'Scheduler');
+create index timestamps on login(LoginTime,LogoutTime);
+create index fullname on patient(fname,lname);
+create index staff_fullname on employee(fname,lname);
+
+
+create table login (
+
+	userID varchar(16),
+	/*privilege ,
+	*/
+	priv privilege,
+	LoginTime time,
+	LogoutTime time,
+	primary key (userID)
+
+);
+
+create table diagnostic (
+
+	ID varchar(16),
+	price decimal(6,2),
+	cat category,
+	primary key (ID)
+
+);
+
+create table patient (
+
+	fname varchar(16),
+	lname varchar(16),
+	address varchar(16),
+	patientID varchar(16)
+
+);
+
+create table employee (
+
+	fname varchar(16),
+	lname varchar(16),
+	staffID varchar(16),
+	job jobtype,
+	primary key (staffID)
+
+);
+
+create table ord (
+
+	orderID varchar(16),
+	customerID varchar(16),
+	staffID varchar(16),
+	diagnosticID varchar(16),
+	results text,
+	primary key (orderID),
+	foreign key (diagnosticID) references diagnostic(ID),
+	foreign key (staffID) references employee(staffID)
+
+);
